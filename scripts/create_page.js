fe15f7cc1e5374b64eec4ee818cadda8f83fed3c
tayutaedomo <tayutaedomo@gmail.com>
2024-05-20 20:23:53 +0900

    Enhance page creation script

--- scripts/create_page.js
+++ scripts/create_page.js
@@ -3,7 +3,14 @@ const { Client } = require('@notionhq/client');
 async function main() {
   const notion = new Client({ auth: process.env.NOTION_KEY });
   const databaseId = process.argv[2];
+
+  // 今日の日付を YYYY-MM-DD の形式で取得する
+  const todayStr = new Date().toISOString().split('T')[0];
+
+  // タイトルを作成する
   const titleSuffix = process.argv[3];
+  const titleDayStr = todayStr.replace(/-/g, '').slice(2);
+  const title = `${titleDayStr} ${titleSuffix}`;
 
   const newPage = await notion.pages.create({
     parent: {
@@ -14,10 +21,18 @@ async function main() {
         title: [
           {
             text: {
-              content: titleSuffix,
+              content: title,
             },
           },
         ],
+      },
+      Date: {
+        type: 'date',
+        date: {
+          start: todayStr,
+          end: null,
+          time_zone: null,
+        },
       },
     },
   });
