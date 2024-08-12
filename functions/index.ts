027e659435bde24bd84fdf0b1a01906a8a5457c0
tayutaedomo <tayutaedomo@gmail.com>
2024-08-12 20:07:05 +0900

    Add gcf notionAuth API

--- functions/index.ts
+++ functions/index.ts
@@ -1,5 +1,13 @@
 import { Request, Response } from 'express';
+import { Client } from '@notionhq/client';
 
 export const helloWorld = (req: Request, res: Response) => {
   res.send('Hello, World!');
+};
+
+export const notionAuth = async (req: Request, res: Response) => {
+  const NOTION_KEY = process.env.NOTION_KEY || '';
+  const notion = new Client({ auth: NOTION_KEY });
+  const response = await notion.users.list({});
+  res.json(response);
 };
